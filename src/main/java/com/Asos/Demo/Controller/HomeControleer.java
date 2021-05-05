ba88619b77ddd297ca2fd133a2af5edeb4728eef
Veeruachar <78130880+Veeruachar@users.noreply.github.com>
2021-05-05 22:58:04 +0530

    Added pathvariable

--- src/main/java/com/Asos/Demo/Controller/HomeControleer.java
+++ src/main/java/com/Asos/Demo/Controller/HomeControleer.java
@@ -1,14 +1,15 @@
 package com.Asos.Demo.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.@PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/greetings")
 public class HomeControleer {
-	@GetMapping("/greet")
-	public String home() {
-		return "Hello, war deployed";
+	@GetMapping("/greet/{name}")
+	public String home(@PathVariable("name") String name) {
+		return name+"Hello, war deployed";
 	}
 }
