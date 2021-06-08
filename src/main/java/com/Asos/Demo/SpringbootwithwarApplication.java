ce7a2d4f1496623f40338b510b9a5904db9bfc45
Veeruachar <78130880+Veeruachar@users.noreply.github.com>
2021-06-08 21:19:57 +0530

    changed from war to jar step2

--- src/main/java/com/Asos/Demo/SpringbootwithwarApplication.java
+++ src/main/java/com/Asos/Demo/SpringbootwithwarApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootwithwarApplication extends SpringBootServletInitializer{
+public class SpringbootwithwarApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootwithwarApplication.class, args);
