c05392ab575d2c4793dfd2ddfa0184629ac6e8ac
Veeruachar <veereshcse.rymec@gmail.com>
2021-09-08 19:24:53 +0530

    added message

--- src/main/java/com/Asos/Demo/Controller/HomeControleer.java
+++ src/main/java/com/Asos/Demo/Controller/HomeControleer.java
@@ -11,6 +11,7 @@ public class HomeControleer {
 	@GetMapping("/greet/{name}")
 	public String home(@PathVariable("name") String name) {
 		System.out.println("Hi india");
+		System.out.println("Mera bharath mahan");
 		return "Captain"+name+" "+"Hello, war deployed";
 	}
 }
